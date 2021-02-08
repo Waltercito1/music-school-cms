2eb2e4a0028bf29196f22853f021821adbdbf1ea
Walter Aab <aabwalter@yahoo.com>
2021-02-08 16:20:43 -0600

    Set up validations to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_many :courses
     has_many :userCourses
+    has_secure_password
+    validates :password, length: {in: 6..50}, confirmation: true
+    validates :username, uniqueness: true, length: {in: 5..30}, exclusion: {in: ["admin", "superadmin", "user"]}
 end
