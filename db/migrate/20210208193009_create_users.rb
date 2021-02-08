4df3813eadae7bb8a9c742fda64eff5e619aebad
Walter Aab <aabwalter@yahoo.com>
2021-02-08 13:51:34 -0600

    Create users table

---
+++ db/migrate/20210208193009_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :user_name
+      t.string :email_address
+      t.string :password_digest
+      t.boolean :instructor, default:false
+
+      t.timestamps null: false
+    end
+  end
+end
