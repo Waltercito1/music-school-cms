7b093b76e5d33bdec1a6aa21e23f6a276185af69
Walter Aab <aabwalter@yahoo.com>
2021-02-09 15:08:58 -0600

    Add fix to session controller for login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,20 @@ class SessionsController < ApplicationController
 
   # POST: /sessions
   post "/login" do
-    binding.pry
+    user = User.find_by_email_address(params["user"]["email_address"])
+    if user && user.authenticate(params["user"]["password"])
+      session["user_id"] = user.id
+      flash[:success] = "Success!"
+      redirect '/courses'
+    else
+      flash[:error] = "Invalid credentials"
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
     has_many :userCourses
     has_secure_password
     validates :password, length: {in: 6..50} , confirmation: true
-    validates :username, uniqueness: true, length: {in: 5..30}, exclusion: {in: ["admin", "superadmin", "user"]}
-    validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :user_name, uniqueness: true, length: {in: 5..30}, exclusion: {in: ["admin", "superadmin", "user"]}
+    validates :email_address, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
 end
