cb1586b0da6f8c9079ad6e0203808bece1428ee6
Walter Aab <aabwalter@yahoo.com>
2021-02-09 17:53:22 -0600

    Add fix relationship in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,4 @@
 class Course < ActiveRecord::Base
-    belongs_to :instructor, class_name: "User" #, foreign_key: "teacher_id"
+    belongs_to :instructor, class_name: "User", foreign_key: "teacher_id"
     validates :name, :description, :level, :term, presence: true
 end
