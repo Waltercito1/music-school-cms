52b9543a908386318902aa240050033c3c18404d
Walter Aab <aabwalter@yahoo.com>
2021-02-13 17:00:40 -0600

    Add fix to new.html.erb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
     belongs_to :instructor, class_name: "User", foreign_key: "teacher_id"
     validates :name, :description, :category, :level, :semester, presence: true
+    validates :name, uniqueness: true
 end
