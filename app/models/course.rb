f430f1f729fd2a5c82f6d5e5a87a3249b2f372c3
Walter Aab <aabwalter@yahoo.com>
2021-02-17 13:58:29 -0600

    Add validation to course models for dropdown menu choices

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ActiveRecord::Base
     belongs_to :user 
-    validates :name, :description, :category, :level, :semester, presence: true
+    #validates :name, :description, :category, :level, :semester, presence: true
+    validates :name, :description, presence: true
     validates :name, uniqueness: true
+    validates :category, :level, :semester, presence: true, exclusion: { in: %w(Choose...), 
+    message: "%{value} is not valid. Please use the dropdown menu to pick one." }
 end
